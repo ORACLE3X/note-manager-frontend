5430d0e3d445294b81393c997c74438a83a7e6e7
ORACLE3X <legendaryoracle8@gmail.com>
2024-11-26 18:24:32 +0100

    Worked on js html form

---
+++ src/js/utils.js
@@ -0,0 +1,6 @@
+const signup = document.getElementById("signup");
+signup.addEventListener('click', (e)=>{
+    e.preventDefault();
+   let password = document.getElementsByName("password")
+   console.log(password);
+})
