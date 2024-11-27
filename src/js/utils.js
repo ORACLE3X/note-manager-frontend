2d205f644a4390fbdd2a8c1ad604e76619751e24
ORACLE3X <legendaryoracle8@gmail.com>
2024-11-27 18:10:01 +0100

    Worked on CSS

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,26 @@
 const signup = document.getElementById("signup");
 signup.addEventListener('click', (e)=>{
+    try {
+        
+    
     e.preventDefault();
-   let password = document.getElementsByName("password")
-   console.log(password);
+
+   let password = document.getElementById("password").value;
+   let email = document.getElementById("email").value;
+   let gender = document.getElementById("gender").value;
+   let track = document.getElementById("track").value;
+//    console.log({password, email});
+if(!password || password ==null)throw new Error("password is required")
+if(gender.toLowerCase() !== "male" && gender.toLowerCase()!=="female") throw new Error("Invalid gender")
+if(track ==="select")throw new Error("select a track")
+    const user = {
+ password,
+ gender,
+ track,
+ email
+    }
+    console.log(user)
+} catch (error) {
+       alert(error.message)
+}
 })
