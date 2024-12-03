3d3f486395e7220bd4b04bde2899e0ceb92f5630
ORACLE3X <legendaryoracle8@gmail.com>
2024-12-03 17:38:21 +0100

    improved on the login and the signup

--- src/API/note.js
+++ src/API/note.js
@@ -1,5 +1,5 @@
-// const  baseUrl ="http://localhost:8000/api/v1";
-const baseUrl = "https://notemanager-1kfi.onrender.com/api/v1"
+const  baseUrl ="http://localhost:8000/api/v1";
+// const baseUrl = "https://notemanager-1kfi.onrender.com/api/v1"
 const header = {
     "content-type": "application/json",
     authorization:`bearer ${localStorage.getItem("acessToken")}`
