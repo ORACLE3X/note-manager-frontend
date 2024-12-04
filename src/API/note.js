180a518b278ce2e99cd6000222971c13a521d269
ORACLE3X <legendaryoracle8@gmail.com>
2024-12-04 17:38:06 +0100

    Log in and saved acessToken to local storage

--- src/API/note.js
+++ src/API/note.js
@@ -1,6 +1,10 @@
 const  baseUrl ="http://localhost:8000/api/v1";
 // const baseUrl = "https://notemanager-1kfi.onrender.com/api/v1"
+const getUserAuth = ()=>{
+    const auth = JSON.parse(localStorage.getItem("auth"));
+    return auth;
+};
 const header = {
     "content-type": "application/json",
-    authorization:`bearer ${localStorage.getItem("acessToken")}`
+    authorization:`bearer ${getUserAuth().accessToken}`
 };
--- src/js/login.js
+++ src/js/login.js
@@ -1,31 +1,35 @@
 const signin = document.getElementById("signin");
-signin.addEventListener('click', async(e)=>{
-  try {
-    
-    e.preventDefault();
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    if(!email) throw new Error("Email is required")
-    if(!password || password ==null)throw new Error("password is required");
-    const user = {
-        email,
-      password,
-    };
-    const url = `${baseUrl}${routes.login}`;
-    const res = await fetch(url, {
-      method:"POST",
-      headers: {
-        'content-type':"application/json"
-      },
-      body: JSON.stringify(user)
-    });
-    const result = await res.json()
-    // console.log(result);
-    if(!res.ok){
-      throw new Error(result.error)
+signin.addEventListener('click', async (e) => {
+    try {
+        e.preventDefault();
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+        if (!email) throw new Error("Email is required")
+        if (!password || password === null) throw new Error("password is required");
+        const user = {
+            email,
+            password,
+        };
+        const url = `${baseUrl}${routes.login}`;
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'content-type': "application/json"
+            },
+            body: JSON.stringify(user)
+        });
+        const result = await res.json();
+        // console.log(result);
+        if (!res.ok) {
+            throw new Error(result.error);
+        }  
+        const {acessToken, refreshToken} = result;
+        const auth = {acessToken, refreshToken};
+        localStorage.setItem("auth", JSON.stringify(auth));
+        alert(result.message)
+        window.location = "../pages/display.html"
+    } catch (error) {
+        console.log(error)
+        alert(error.message)
     }
-    alert(result.message);
-  } catch (error) {
-    alert(error.message)
-  }
 })
