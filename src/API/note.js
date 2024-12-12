d49879478940acf811eea6878bbaa8d6fc7722f3
ORACLE3X <legendaryoracle8@gmail.com>
2024-12-12 19:26:57 +0100

    worked on dashboard html and css

--- src/API/note.js
+++ src/API/note.js
@@ -4,6 +4,9 @@ const getUserAuth = ()=>{
     const auth = JSON.parse(localStorage.getItem("auth"));
     return auth;
 };
+getUserInfo = () => {
+    return JSON.parse(localStorage.getItem("userInfo"));
+}
 const header = {
     "content-type": "application/json",
     authorization:`bearer ${getUserAuth().accessToken}`
